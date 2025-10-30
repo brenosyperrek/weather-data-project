525aca48f44767bb007a317ed929333056c0ec8a
Breno Syperrek <brenosyperrek@gmail.com>
2025-10-30 15:06:20 -0300

    comitando arquivo insert_records.py com o código para a gravação do registro na tabela

--- api-request/insert_records.py
+++ api-request/insert_records.py
@@ -40,5 +40,45 @@ def create_table(conn):
         print(f"Erro ao criar a tabela: {e}")
         raise
     
-conn = connect_to_db()
-create_table(conn)
+def insert_records(conn, data):
+    print("Inserindo dados de clima na tabela...")
+    try:
+        weather = data['current']
+        location = data['location']
+        cursor = conn.cursor()
+        cursor.execute('''
+            INSERT INTO weather_schema.weather_data (
+                city,
+                temperature,
+                weather_descriptions,
+                wind_speed,
+                time,
+                inserted_at,
+                utc_offset
+            ) VALUES (%s, %s, %s, %s, %s, NOW(), %s)
+        ''', (
+            location['name'],
+            weather['temperature'],
+            weather['weather_descriptions'][0],
+            weather['wind_speed'],
+            location['localtime'],
+            location['utc_offset']
+        ))
+        conn.commit()
+        print("Dados inseridos com sucesso.")
+    except psycopg2.Error as e:
+        print(f"Erro ao inserir os dados: {e}")
+        raise
+                
+def main():
+    try:
+        data = mock_fetch_weather_data()
+        conn = connect_to_db()
+        create_table(conn)
+        insert_records(conn, data)
+    except Exception as e:
+        print(f"Erro no processo principal: {e}")
+    finally:
+        if 'conn' in locals():
+            conn.close()
+            print("Conexão com o banco de dados fechada.")
